bb456092a973ddbf87cf371aec06a26ce7043b81
Kockstik <kostyadi04@mail.ru>
2026-05-15 15:51:34 +0700

    Add model for chats

---
+++ Chatbox/Entity/ChatDTO.swift
@@ -0,0 +1,16 @@
+//
+//  Chat.swift
+//  Chatbox
+//
+//  Created by admin on 14.05.2026.
+//
+
+import UIKit
+
+struct ChatDTO {
+    var image: UIImage
+    var name: String
+    var lastMessage: String
+    var date: Date
+    var unreadCount: Int
+}
